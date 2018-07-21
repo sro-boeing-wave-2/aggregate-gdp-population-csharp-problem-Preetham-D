7f4090a3a8c4b7e7e9726796e479ca90a16d1904
Preetham D <preethamdbangera@gmail.com>
2018-07-21 17:01:09 +0530

    Fixed path 2

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -15,7 +15,7 @@ namespace AggregateGDPPopulation.Tests
         public void Test1()
         {
             Class1.AggregateCalcultion();
-            var actual = File.ReadAllText(@"../../../..AggregateGDPPopulation/data/output.json");
+            var actual = File.ReadAllText(@"../../../../AggregateGDPPopulation/data/output.json");
             var expected = File.ReadAllText(@"../../../expectedOutput.json");
             JObject actualJson = JObject.Parse(actual);
             JObject expectedJson = JObject.Parse(expected);
